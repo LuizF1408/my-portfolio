921cb6371bb5c758efe97006a79570a8e5b8e98a
Luiz <liko.12@hotmail.com>
2020-12-22 23:05:55 -0300

    inital commit

---
+++ apiFlavia/server.js
@@ -0,0 +1,13 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = (process.env.PORT || 3000);
+const app = express();
+
+
+
+app.use(bodyParser.json());
+app.listen(port,() =>{
+    console.log(`Server is ON ${port}`)
+});
